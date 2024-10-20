9bc2fd8f6b53c43ecf58d1d8a872598ff9588fb8
togwat <jyfuyy@gmail.com>
2024-10-21 01:13:09 +1300

    DbContext + migrations
    Using Postgresql

---
+++ EStoreAPI.Server/Data/EStoreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EStoreAPI.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EStoreAPI.Server.Data
+{
+    public class EStoreDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+
+        public EStoreDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // connection string from appsettings.json
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebAPIDatabase"));
+        }
+    }
+}
