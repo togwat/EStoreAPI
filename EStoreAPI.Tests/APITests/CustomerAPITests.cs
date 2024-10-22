147af3663bd26451d1c1d1275e13f43703f4cf72
togwat <jyfuyy@gmail.com>
2024-10-22 19:47:01 +1300

    More tests for CustomerAPI
    Added TestGetEmptyCustomers, TestGetCustomersByValidId, TestGetCustomersByInvalidId

--- EStoreAPI.Tests/APITests/CustomerAPITests.cs
+++ EStoreAPI.Tests/APITests/CustomerAPITests.cs
@@ -36,7 +36,67 @@ namespace EStoreAPI.Tests.APITests
             var okResult = Assert.IsType<OkObjectResult>(result.Result);    // returns 200 Ok
             var customersResult = Assert.IsAssignableFrom<ICollection<Customer>>(okResult.Value);   // return type ICollection<Customer>
             Assert.Equal(5, customersResult.Count);   // returns 5 customers
+        }
 
+        [Fact]
+        public async Task TestGetEmptyCustomers()
+        {
+            // arrange
+            _repo.Setup(r => r.GetCustomersAsync()).ReturnsAsync(new List<Customer>());
+
+            // act
+            var result = await _Controller.GetAllCustomersAsync();
+
+            // assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);    // returns 200 Ok
+            var customersResult = Assert.IsAssignableFrom<ICollection<Customer>>(okResult.Value);   // return type ICollection<Customer>
+            Assert.Empty(customersResult);   // returns empty list
+        }
+
+        // GET: api/Customers/{id}
+        [Fact]
+        public async Task TestGetCustomersByValidId()
+        {
+            var customers = _fixture.CreateMany<Customer>(5);
+            foreach(var customer in customers)
+            {
+                _repo.Setup(r => r.GetCustomerByIdAsync(customer.CustomerId)).ReturnsAsync(customer);
+            }
+
+            // act
+            foreach (var customer in customers)
+            {
+                var result = await _Controller.GetCustomerAsync(customer.CustomerId);
+
+                // assert
+                var okResult = Assert.IsType<OkObjectResult>(result.Result);    // returns 200 Ok
+                var customerResult = Assert.IsAssignableFrom<Customer>(okResult.Value); // return type Customer
+                Assert.Equal(customer.CustomerId, customerResult.CustomerId);   // matching id        
+            }
+        }
+
+        [Fact]
+        public async Task TestGetCustomersByInvalidId()
+        {
+            // arrange
+            var customers = _fixture.CreateMany<Customer>(5);
+            foreach (var customer in customers)
+            {
+                _repo.Setup(r => r.GetCustomerByIdAsync(customer.CustomerId)).ReturnsAsync(customer);
+            }
+
+            int validId = customers.Max(x => x.CustomerId);
+
+            IEnumerable<int> invalidIds = new List<int> { -1, validId + 1 };
+
+            foreach (int id in invalidIds)
+            {
+                // act
+                var invalidResult = await _Controller.GetCustomerAsync(id);
+
+                // assert
+                var notFoundResult = Assert.IsType<NotFoundObjectResult>(invalidResult.Result); // returns 404 Not found
+            }
         }
     }
 }
