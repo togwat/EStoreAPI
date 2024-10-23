ea27f01109c4a0af97a106b33fa12d975b5cb67b
togwat <jyfuyy@gmail.com>
2024-10-23 17:57:27 +1300

    Create DeviceAPITests.cs

---
+++ EStoreAPI.Tests/APITests/DeviceAPITests.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using Moq;
+using EStoreAPI.Server.Controllers;
+using EStoreAPI.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EStoreAPI.Tests.APITests
+{
+    public class DeviceAPITests : APITests<DevicesController>
+    {
+        // GET: api/Devices
+        [Fact]
+        public async Task TestGetDevices()
+        {
+            // arrange
+            var devices = _fixture.CreateMany<Device>(5).ToList();
+            _repo.Setup(r => r.GetDevicesAsync()).ReturnsAsync(devices);
+
+            // act
+            var result = await _controller.GetAllDevicesAsync();
+
+            // assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);    // returns 200 ok
+            var devicesResult = Assert.IsAssignableFrom<ICollection<Device>>(okResult.Value);  // return type ICollection<Device>
+            Assert.Equal(5, devicesResult.Count);
+        }
+    }
+}
