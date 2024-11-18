abf8bfca5f05a8ed3e47378dad1e7c8b4cfe2087
togwat <jyfuyy@gmail.com>
2024-11-18 15:48:59 +1300

    Resolved test fixture problems
    With help from https://daninacan.com/using-autofixture-with-ef-core-without-circular-references/

--- EStoreAPI.Tests/APITests/APITests.cs
+++ EStoreAPI.Tests/APITests/APITests.cs
@@ -3,6 +3,8 @@ using Moq;
 using AutoFixture.AutoMoq;
 using EStoreAPI.Server.Data;
 using Microsoft.AspNetCore.Mvc;
+using AutoFixture.Kernel;
+using System.Reflection;
 
 namespace EStoreAPI.Tests.APITests
 {
@@ -14,9 +16,56 @@ namespace EStoreAPI.Tests.APITests
 
         public APITests()
         {
-            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _fixture = new Fixture()
+                .Customize(new AutoMoqCustomization())
+                .Customize(new NoCircularReferencesCustomization())
+                .Customize(new IgnoreVirtualMembersCustomization());
+
             _repo = _fixture.Freeze<Mock<IEStoreRepo>>();
             _controller = (T)Activator.CreateInstance(typeof(T), _repo.Object);
+        }
+    }
+
+    // resolves the issue with fixture circular references
+    public class NoCircularReferencesCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(behavior => fixture.Behaviors.Remove(behavior));
+        }
+    }
+
+    public class IgnoreVirtualMembersCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customizations.Add(new IgnoreVirtualMembers());
+        }
+    }
+
+    public class IgnoreVirtualMembers : ISpecimenBuilder
+    {
+        public object? Create(object request, ISpecimenContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var propertyInfo = request as PropertyInfo;
+            if (propertyInfo == null)
+            {
+                return new NoSpecimen();
+            }
+
+            if (propertyInfo.GetMethod != null && propertyInfo.GetMethod.IsVirtual)
+            {
+                return null;
+            }
+
+            return new NoSpecimen();
         }
     }
 }
