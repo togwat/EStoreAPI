7387bea2d8ec3be7e9b9f3bee253051b55447202
togwat <jyfuyy@gmail.com>
2024-11-19 21:42:48 +1300

    Added test get job by id

--- EStoreAPI.Tests/APITests/JobAPITests.cs
+++ EStoreAPI.Tests/APITests/JobAPITests.cs
@@ -24,5 +24,36 @@ namespace EStoreAPI.Tests.APITests
             var jobsResult = Assert.IsAssignableFrom<ICollection<Job>>(okResult.Value); // return type ICollection<Job>
             Assert.Equal(5, jobsResult.Count);  // returns 5 jobs
         }
+
+        // GET: api/Jobs/{id}
+        [Theory]
+        [InlineData(1)]     // valid id
+        [InlineData(-1)]    // invalid id
+        [InlineData(2)]     // invalid id
+        public async Task TestGetJobsById(int id)
+        {
+            // arrange
+            Job job = _fixture.Build<Job>()
+                                .With(j => j.JobId, 1)
+                                .Create();
+            _repo.Setup(r => r.GetJobByIdAsync(1)).ReturnsAsync(job);
+
+            // act
+            var result = await _controller.GetJobAsync(id);
+
+            // assert
+            // valid id
+            if (id == 1)
+            {
+                var okResult = Assert.IsType<OkObjectResult>(result.Result);    // returns 200 ok
+                var jobResult = Assert.IsAssignableFrom<Job>(okResult.Value);   // return type job
+                Assert.Equal(job.JobId, jobResult.JobId);   // matching id
+            }
+            // invalid id
+            else
+            {
+                Assert.IsType<NotFoundObjectResult>(result.Result); // returns 404 not found
+            }
+        }
     }
 }
