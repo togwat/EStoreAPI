33fbab333b95b71f89f5418b651ca4e0d7d78add
togwat <jyfuyy@gmail.com>
2024-11-19 13:00:12 +1300

    Create JobAPITests.cs

---
+++ EStoreAPI.Tests/APITests/JobAPITests.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using Moq;
+using EStoreAPI.Server.Controllers;
+using EStoreAPI.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EStoreAPI.Tests.APITests
+{
+    public class JobAPITests : APITests<JobsController>
+    {
+        // GET: api/Jobs
+        [Fact]
+        public async Task TestGetJobs()
+        {
+            // arrange
+            var jobs = _fixture.CreateMany<Job>(5).ToList();
+            _repo.Setup(r => r.GetJobsAsync()).ReturnsAsync(jobs);
+
+            // act
+            var result = await _controller.GetAllJobsAsync();
+
+            // assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);    // returns 200 ok
+            var jobsResult = Assert.IsAssignableFrom<ICollection<Job>>(okResult.Value); // return type ICollection<Job>
+            Assert.Equal(5, jobsResult.Count);  // returns 5 jobs
+        }
+    }
+}
