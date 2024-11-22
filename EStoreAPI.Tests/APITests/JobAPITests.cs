dcc6364e9fdf03e451f767b651f227bc4e9e3e23
togwat <jyfuyy@gmail.com>
2024-11-22 15:47:36 +1300

    Added test update job

--- EStoreAPI.Tests/APITests/JobAPITests.cs
+++ EStoreAPI.Tests/APITests/JobAPITests.cs
@@ -107,5 +107,67 @@ namespace EStoreAPI.Tests.APITests
                 [1, 1, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem>()],    // invalid problems
                 [-1, -1, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem>()],  // invalid combined
             ];
+
+        // PUT: api/Jobs/update{id}
+        [Theory]
+        [MemberData(nameof(UpdateJobData))]
+        public async Task TestUpdateJob(int id, int customerId, int deviceId, DateTime receiveTime, ICollection<Problem> problems)
+        {
+            Job oldJob = _fixture.Build<Job>()
+                                .With(j => j.JobId, 1)
+                                .Create();
+            Job newJob = _fixture.Build<Job>()
+                                .With(j => j.JobId, 1)
+                                .With(j => j.CustomerId, customerId)
+                                .With(j => j.DeviceId, deviceId)
+                                .With(j => j.ReceiveTime, receiveTime)
+                                .With(j => j.Problems, problems)
+                                .Create();
+            // valid id
+            if (id == 1)
+            {
+                _repo.Setup(r => r.UpdateJobAsync(newJob)).Returns(Task.CompletedTask);
+            }
+            // not found id
+            else
+            {
+                _repo.Setup(r => r.UpdateJobAsync(newJob)).ThrowsAsync(new KeyNotFoundException("Job not found."));
+            }
+
+            // act
+            var result = await _controller.UpdateJobByIdAsync(id, newJob);
+
+            // assert
+            // valid id
+            if (id == 1)
+            {
+                // valid data
+                if (customerId == 2 && deviceId == 2 && problems.Count >= 1)
+                {
+                    Assert.IsType<NoContentResult>(result); // returns 204 no content
+                }
+                // invalid data
+                else
+                {
+                    var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);   // returns 404 not found
+                    Assert.Equal("Job not found.", notFoundResult.Value);   // matching error message
+                }
+            }
+            // invalid id
+            else
+            {
+                Assert.IsType<BadRequestResult>(result);    // returns 400 bad request
+            }
+        }
+
+        public static IEnumerable<object[]> UpdateJobData =>
+            [
+                [1, 2, 2, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem> { new(), new() }],   // valid
+                [2, 2, 2, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem> { new(), new() }],   // invalid id
+                [1, -1, 2, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem> { new(), new() }],  // valid id. invalid data
+                [1, 2, -1, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem> { new(), new() }],  // valid id. invalid data
+                [1, 2, 2, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem>()],     // valid id. invalid data
+                [2, -1, -1, new DateTime(2024, 1, 1, 12, 0, 0), new List<Problem>()],     // invalid everything
+            ];
     }
 }
