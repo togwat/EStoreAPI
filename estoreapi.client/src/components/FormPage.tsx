f3b6c8fd0d77ad7459064594b5764a5ef0748361
togwat <jyfuyy@gmail.com>
2024-12-15 01:09:21 +1300

    Form

--- estoreapi.client/src/components/FormPage.tsx
+++ estoreapi.client/src/components/FormPage.tsx
@@ -4,7 +4,26 @@ function FormPage() {
     return (
         <div>
             <MenuButton/>
-            <p>Form</p>
+            <h1>E-Store Repair Job Form</h1>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input name="name" />
+                <label htmlFor="phone">Phone number</label>
+                <input name="phone" />
+                <label htmlFor="phone2">Secondary phone number</label>
+                <input name="phone2" />
+                <label htmlFor="email">Email</label>
+                <input name="email" />
+                <label htmlFor="address">Address</label>
+                <input name="address" />
+                <label htmlFor="Device">Device</label>
+                <input name="device" />
+                <label htmlFor="notes">Notes</label>
+                <textarea name="notes" />
+                <label htmlFor="price">Estimated price</label>
+                <input name="price" />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
