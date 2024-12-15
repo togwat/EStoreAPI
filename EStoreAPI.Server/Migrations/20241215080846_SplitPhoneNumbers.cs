a64505fb79c5670f5ee0731fbe8cf39d531f6bb9
togwat <jyfuyy@gmail.com>
2024-12-15 21:09:36 +1300

    Apply changes as new migration

---
+++ EStoreAPI.Server/Migrations/20241215080846_SplitPhoneNumbers.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EStoreAPI.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SplitPhoneNumbers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumbers",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumberSecondary",
+                table: "Customers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumberSecondary",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "PhoneNumbers",
+                table: "Customers",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+        }
+    }
+}
