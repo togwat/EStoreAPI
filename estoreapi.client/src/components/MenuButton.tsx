e45d99bf8838b5b6f55889d3dd723145ff5fb926
Max <61611260+togwat@users.noreply.github.com>
2026-03-10 13:28:40 +1300

    Menu button always top-left

--- estoreapi.client/src/components/MenuButton.tsx
+++ estoreapi.client/src/components/MenuButton.tsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
+import './MenuButton.css';
 
 function MenuButton() {
       return (
-          <Link to="/">
+          <Link to="/" className="back-button">
               <button>Back</button>
         </Link>
     );
