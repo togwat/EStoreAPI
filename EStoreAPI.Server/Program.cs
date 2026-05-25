0751685d6f8f57c025f98ce81c0b7a66f379f4d6
Max <61611260+togwat@users.noreply.github.com>
2026-05-25 23:56:25 +1200

    Quick circular reference fix

--- EStoreAPI.Server/Program.cs
+++ EStoreAPI.Server/Program.cs
@@ -4,8 +4,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+// prevent cycles in responses
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
