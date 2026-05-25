153e666aee278f1ea566fc9c8e5e61a2c71b9882
Max <61611260+togwat@users.noreply.github.com>
2026-05-25 19:18:10 +1200

    New default sizes

--- estoreapi.client/src/components/assistant-ui/assistant-sidebar.tsx
+++ estoreapi.client/src/components/assistant-ui/assistant-sidebar.tsx
@@ -9,13 +9,16 @@ import { Thread } from "src/components/assistant-ui/thread";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 
+const defaultChatWidth = 25;
+const defaultChatWidthMobile = 50;
+
 export const AssistantSidebar: FC<PropsWithChildren> = ({ children }) => {
   const isMobile = useIsMobile();
   return (
-    <ResizablePanelGroup orientation={isMobile ? "vertical" : "horizontal"}>
-      <ResizablePanel>{children}</ResizablePanel>
+    <ResizablePanelGroup key={isMobile ? "vertical" : "horizontal"} orientation={isMobile ? "vertical" : "horizontal"}>
+      <ResizablePanel defaultSize={isMobile ? 100 - defaultChatWidthMobile : 100 - defaultChatWidth}>{children}</ResizablePanel>
       <ResizableHandle />
-      <ResizablePanel>
+      <ResizablePanel defaultSize={isMobile ? defaultChatWidthMobile : defaultChatWidth}>
         <Thread />
       </ResizablePanel>
     </ResizablePanelGroup>
