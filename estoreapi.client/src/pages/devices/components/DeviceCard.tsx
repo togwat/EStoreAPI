bd7a566894783df1f1e87e1a1bd5f29da2f968f0
Max <61611260+togwat@users.noreply.github.com>
2026-06-01 01:01:38 +1200

    Clickable device card

--- estoreapi.client/src/pages/devices/components/DeviceCard.tsx
+++ estoreapi.client/src/pages/devices/components/DeviceCard.tsx
@@ -14,11 +14,18 @@ export type Device = {
     type: string
 }
 
-export function DeviceCard({ device }: { device: Device }) {
+export function DeviceCard({ device, onClick }: { device: Device; onClick?: () => void }) {
     const isMobile = useIsMobile();
 
     return (
-        <Card size="sm" className="w-full">
+        <Card
+            size="sm"
+            className={cn("w-full border border-border cursor-pointer hover:border-foreground/50 transition-all focus:bg-accent focus:border-ring focus:ring-2 focus:ring-ring/50 focus:text-accent-foreground")}
+            onClick={onClick}
+            role={"button"}
+            tabIndex={0}
+            // onKeyDown={(e) => { if (e.key === "Enter") onClick() }}
+        >
             <CardHeader className={cn(isMobile && "flex! flex-row gap-2")}>
                 <CardTitle>{device.name}</CardTitle>
                 <CardDescription>{device.type}</CardDescription>
