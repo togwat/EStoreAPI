d68342d38d04e346303f968b8bae721cace22cd2
Max <61611260+togwat@users.noreply.github.com>
2026-06-03 14:53:03 +1200

    Mobile scaling

--- estoreapi.client/src/pages/404/components/Eyes.tsx
+++ estoreapi.client/src/pages/404/components/Eyes.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect, useState } from "react";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 function getAngle(cx: number, cy: number, ex: number, ey: number) {
     const dy = ey - cy;
@@ -11,6 +12,9 @@ function getAngle(cx: number, cy: number, ex: number, ey: number) {
 export default function Eyes() {
     const rectRef = useRef<HTMLDivElement>(null);
     const [eyeDeg, setEyeDeg] = useState<number | null>(null);
+    const isMobile = useIsMobile();
+
+    const scale = isMobile ? 0.5 : 1;
 
     useEffect(() => {
         document.addEventListener("mousemove", handleMouseMove);
@@ -38,14 +42,16 @@ export default function Eyes() {
     // eye & pupil
     function Eye() {
         return (
-            <div className="rounded-full border-8 border-foreground bg-transparent w-40 h-40" style={{ transform: `rotate(${eyeDeg}deg)` }}>
+            <div
+                className="rounded-full border-8 border-foreground bg-transparent w-40 h-40"
+                style={{ transform: `rotate(${eyeDeg}deg)` }}>
                 <div className="rounded-full bg-foreground w-20 h-20 m-3" />
             </div>
         )
     }
 
     return (
-        <div className="flex flex-row gap-16" ref={rectRef}>
+        <div className="flex flex-row gap-8" style={{ scale: String(scale) }} ref={rectRef}>
             <Eye />
             <Eye />
         </div>
