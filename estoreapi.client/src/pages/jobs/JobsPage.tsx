1be2f4f10eada21c46b45d62036a114770957c70
Max <61611260+togwat@users.noreply.github.com>
2026-06-03 17:35:30 +1200

    JobsPage skeleton

--- estoreapi.client/src/pages/jobs/JobsPage.tsx
+++ estoreapi.client/src/pages/jobs/JobsPage.tsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react';
 import { useIsMobile } from '@/hooks/use-mobile';
+import { PanelDrawer } from '@/components/PanelDrawer';
+import { getJobs, Job } from '@/api/jobs';
 
 export default function JobsPage({ title }: { title: string }) {
     const isMobile = useIsMobile();
+    const [jobs, setJobs] = useState<Job[]>([]);                        // @ts-ignore
+    const [selectedJob, setSelectedJob] = useState<Job | null>(null);   // @ts-ignore
+
+    useEffect(() => {
+        getJobs().then(setJobs);
+    }, []);
+    
+    console.log(jobs);
 
     return (
-        <div>
-            { !isMobile && <h1>{title}</h1> }
-        </div>
+        <PanelDrawer
+            open={selectedJob !== null}
+            drawerContent={selectedJob && (
+                <div className="w-full h-full overflow-auto">
+                </div>
+            )}
+        >
+            {/** main body */}
+            {isMobile
+                // mobile
+                ? <div className="flex flex-col gap-2">
+                </div>
+                // desktop
+                : <div className="p-8">
+                    <h1>{title}</h1>
+                </div>
+            }
+        </PanelDrawer>
     );
 }
