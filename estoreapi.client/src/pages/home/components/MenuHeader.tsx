8ee4f9c581cbb0b3faca178e849c51bddff0c366
Max <61611260+togwat@users.noreply.github.com>
2026-06-07 16:09:06 +1200

    Mobile MenuHeader resize

--- estoreapi.client/src/pages/home/components/MenuHeader.tsx
+++ estoreapi.client/src/pages/home/components/MenuHeader.tsx
@@ -6,8 +6,8 @@ export default function MenuHeader() {
     const isMobile = useIsMobile();
     return (
         <div className={`flex items-center gap-2 flex-wrap ${isMobile && "mx-auto"}`}>
-            <img src={themeLogos[getTheme()]} className="h-7" />
-            <h1 className="m-0 text-2xl">Management Console</h1>
+            <img src={themeLogos[getTheme()]} className={isMobile ? "h-6" : "h-7"} />
+            <h1 className={`m-0 ${isMobile ? "text-xl" : "text-2xl"}`}>Management Console</h1>
         </div>
     );
 }
