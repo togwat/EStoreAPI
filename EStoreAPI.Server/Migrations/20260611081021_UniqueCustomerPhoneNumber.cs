c9d0069b60428ccd11fcf555be30bcf2b1199a87
Max <61611260+togwat@users.noreply.github.com>
2026-06-11 20:11:23 +1200

    Phone number constraint migration

---
+++ EStoreAPI.Server/Migrations/20260611081021_UniqueCustomerPhoneNumber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EStoreAPI.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueCustomerPhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_PhoneNumber",
+                table: "Customers",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_PhoneNumber",
+                table: "Customers");
+        }
+    }
+}
