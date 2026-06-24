3bb272fc3ab5ff091b5e3fe2afde900962e70569
Max <61611260+togwat@users.noreply.github.com>
2026-06-24 15:53:26 +1200

    AddWarranty EF migration

---
+++ EStoreAPI.Server/Migrations/20260624035242_AddWarranty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EStoreAPI.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWarranty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WarrantyOfJobId",
+                table: "Jobs",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WarrantyOfJobId",
+                table: "Jobs");
+        }
+    }
+}
