646cab841a41a1218d0e4aaf50241357bdd40342
Max <61611260+togwat@users.noreply.github.com>
2026-06-26 00:29:36 +1200

    EF migration
    In the migration, I am moving the price column to the new partsPrice column, as the old db had price represent parts price.

---
+++ EStoreAPI.Server/Migrations/20260625122644_AddPartsPrice.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EStoreAPI.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPartsPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PartsPrice",
+                table: "Problems",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            // The old Price column represented partsPrice.
+            // In this migration, move the old Price column to partsPrice and leave Price at 0
+            migrationBuilder.Sql(@"UPDATE ""Problems"" SET ""PartsPrice"" = ""Price"", ""Price"" = 0;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Restore Price from PartsPrice before dropping the column
+            migrationBuilder.Sql(@"UPDATE ""Problems"" SET ""Price"" = ""PartsPrice"";");
+
+            migrationBuilder.DropColumn(
+                name: "PartsPrice",
+                table: "Problems");
+        }
+    }
+}
