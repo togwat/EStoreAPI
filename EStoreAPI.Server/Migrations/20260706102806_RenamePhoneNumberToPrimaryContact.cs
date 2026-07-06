0724d9956a0f0b7680630af2e569d41c886aa932
Max <61611260+togwat@users.noreply.github.com>
2026-07-06 23:14:05 +1200

    Apply migration
    With custom up down methods

---
+++ EStoreAPI.Server/Migrations/20260706102806_RenamePhoneNumberToPrimaryContact.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EStoreAPI.Server.Migrations
+{
+    public partial class RenamePhoneNumberToPrimaryContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(name: "PhoneNumber", table: "Customers", newName: "PrimaryContact");
+            migrationBuilder.RenameColumn(name: "PhoneNumberSecondary", table: "Customers", newName: "PhoneNumber");
+            migrationBuilder.RenameIndex(name: "IX_Customers_PhoneNumber", table: "Customers", newName: "IX_Customers_PrimaryContact");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(name: "IX_Customers_PrimaryContact", table: "Customers", newName: "IX_Customers_PhoneNumber");
+            migrationBuilder.RenameColumn(name: "PhoneNumber", table: "Customers", newName: "PhoneNumberSecondary");
+            migrationBuilder.RenameColumn(name: "PrimaryContact", table: "Customers", newName: "PhoneNumber");
+        }
+    }
+}
